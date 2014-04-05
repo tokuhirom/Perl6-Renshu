4272a7c7099e1494bb66d142247393744edeb91c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-04-05 21:56:46 +0900

    use Test::Base::Less

--- nqp-lisp/test.pl
+++ nqp-lisp/test.pl
@@ -5,15 +5,23 @@ use utf8;
 use 5.010000;
 use autodie;
 use Capture::Tiny qw(capture);
-use Test::More;
+use Test::Base::Less;
 
-is(run('(print (+ 5 4))'), 9);
-is(run('(print (+ 5 4 3))'), 12);
+filters {
+    input    => ['chomp'],
+    expected => ['chomp'],
+};
+
+for (blocks) {
+    my $expected = $_->expected;
+    $expected =~ s/\n\z//;
+    is(eval_lisp($_->input), $expected, $_->input);
+}
 
 done_testing;
 exit 0;
 
-sub run {
+sub eval_lisp {
     my $code = shift;
 
     my ($stdout, $stderr, $exit) = capture {
@@ -22,3 +30,18 @@ sub run {
     diag $stderr if $stderr;
     return $stdout;
 }
+
+__END__
+
+===
+--- input
+(print (+ 5 4))
+--- expected
+9
+
+===
+--- input
+(print (+ 5 4 3))
+--- expected
+12
+
